ab6bfce6754db8a7f0ff190d3dc09fe52d03c996
shin9898 <shintech9898@gmail.com>
2024-11-24 11:08:38 +0900

    header実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :user_experience_id])
   end
 end
