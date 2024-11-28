f32f4a2402d712db0a9152dd5f9ffd2ef6dcc9fd
shin9898 <shintech9898@gmail.com>
2024-11-28 23:24:19 +0900

    エラーの日本語表示と単体テスト準備

--- app/models/user_experience.rb
+++ app/models/user_experience.rb
@@ -2,7 +2,12 @@ class UserExperience < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '未経験者' },
-    { id: 3, name: '経験者' },
+    { id: 3, name: '初心者' },
+    { id: 4, name: '初級者' },
+    { id: 5, name: '中級者' },
+    { id: 6, name: '実務経験1年〜5年' },
+    { id: 7, name: '実務経験6年〜10年' },
+    { id: 8, name: '実務経験10年以上' }
   ]
 
   include ActiveHash::Associations
