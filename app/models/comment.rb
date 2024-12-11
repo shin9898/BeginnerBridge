56729bc765b30bb521a93d791374a03ca868c084
shin9898 <shintech9898@gmail.com>
2024-12-11 22:35:29 +0900

    コメント欄の表示修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
+  before_save :sanitize_content
+
   belongs_to :post
   belongs_to :user
+
+  validates :content, presence: true, length: { maximum: 500 }
+
+  private
+
+  def sanitize_content
+    # 両端の空白や余分な改行を削除
+    self.content = content.strip.gsub(/\n+/, "\n")
+  end
+
 end
