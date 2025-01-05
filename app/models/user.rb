e84c26d2c4207657ff9ec0fd1681fda4dba01289
shin9898 <shintech9898@gmail.com>
2025-01-05 18:47:27 +0900

    passwordのバリデーションの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,7 @@ class User < ApplicationRecord
                      format: { with: /\A[\p{Hiragana}\p{Katakana}\p{Han}\w]+\z/, 
                                message: "は漢字、ひらがな、カタカナ、英数字（組み合わせでも単体でも可）で入力してください" }
   validates :user_experience_id, numericality: { other_than: 1, message: "が無効なカテゴリーです"}
-  validates :password, presence: true, length: { minimum: 8 },
-                     format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]+\z/, message: "は半角英数字混合で8文字以上にしてください" },
-                     confirmation: { case_sensitive: true }, on: :create
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
   validates :password_confirmation, presence: true, on: :create
   validates :affiliation, length: { maximum: 100 }, allow_blank: true
   validates :site_url, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "URLが無効です" }, allow_blank: true
