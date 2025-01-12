4057fe00e43ff45f2491348d9742d52230c0f088
shin9898 <shintech9898@gmail.com>
2025-01-12 17:46:11 +0900

    一目で何の投稿か分かるように修正

--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,7 +1,7 @@
 class PostCategory < ActiveHash::Base
   self.data = [
     { id: 1, name: '選択してください' },
-    { id: 2, name: '質問' },
+    { id: 2, name: '技術的な質問' },
     { id: 3, name: '意見交換' },
     { id: 4, name: 'その他質問'}
   ]
